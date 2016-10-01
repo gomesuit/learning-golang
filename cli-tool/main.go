1da23847ba9fca2f3a63ffbd5f16daff71273f44
gomesuit <takashi@inouetakashi-no-MacBook.local>
2016-10-01 19:23:21 +0900

    save

--- cli-tool/main.go
+++ cli-tool/main.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-
 	const defaultPort = 3000
 	var port int
 	flag.IntVar(&port, "port", defaultPort, "port to use")
+	flag.IntVar(&port, "p", defaultPort, "port to use (short)")
 	flag.Parse()
 	fmt.Println(port)
 }
